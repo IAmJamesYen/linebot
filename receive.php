5e4c5ac247ed73c81198926e90f5f3ca33f28940
IAmJamesYen <jam@cht.com.tw>
2018-01-09 15:50:28 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -125,7 +125,7 @@
 						array (
 							"image" => array (
 								"source" => array (
-									"imageUri" => "http://139.59.123.8/chtChatBot/20180109_LineBot/".$objID.".jpeg"
+									"imageUri" => "http://139.59.123.8/chtChatBot/JamesYen_LineBot/".$objID.".jpeg"
 								)
 							),
 							"features" => array (
