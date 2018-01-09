0e722ab4097b08458a5020b414ce5614b1e4bf88
IAmJamesYen <jam@cht.com.tw>
2018-01-09 14:12:17 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -4,8 +4,9 @@
  //產生回傳給line server的格式
  $sender_userid = $json_obj->events[0]->source->userId;
  $sender_txt = $json_obj->events[0]->message->text;
+ $sender_replyToken = $json_obj->events[0]->replyToken;
  $response = array (
-				"to" => $sender_userid,
+				"replyToken" => $sender_replyToken,
 				"messages" => array (
 					array (
 						"type" => "text",
@@ -19,7 +20,7 @@
  //回傳給line server
  $header[] = "Content-Type: application/json";
  $header[] = "Authorization: Bearer usKtkP3/syVWHqYd3ZMgjcG3xR3jf9tCquF9Sm75xgDKT04OXRZxtaJl19I5WM0UIWHt3Y5bbrEUSjAuMkW38+MLvTdqfqCsI5RiFDI57WJyOus7PcszC6+xQOtRz5wlYmFf2sepskoqf9gmaU9TIgdB04t89/1O/w1cDnyilFU=";
- $ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
+ $ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
